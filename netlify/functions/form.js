32a6e85c7a2de3b92f26d58df6e77fd85d7f8bd0
sam-ktown <116414057+sam-ktown@users.noreply.github.com>
2022-10-22 01:56:11 -0700

    Update form.js

--- netlify/functions/form.js
+++ netlify/functions/form.js
@@ -9,6 +9,8 @@ exports.handler = async (event, context) => {
   // Guard clause checks for first and last name,
   // and returns early if they are not found
   if (!body.first || !body.last) {
+    console.log("Name missing");
+    
     // Sends a HTTP bad request error code
     //return res.status(400).json({ data: 'First or last name not found' })
     return {
