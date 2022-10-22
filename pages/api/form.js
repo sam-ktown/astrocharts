9aae99d0822e5328b851451aac97d3538fc7423e
sam-ktown <116414057+sam-ktown@users.noreply.github.com>
2022-10-21 23:32:23 -0700

    Update form.js

--- pages/api/form.js
+++ pages/api/form.js
@@ -10,7 +10,7 @@ export default function handler(req, res) {
   // and returns early if they are not found
   if (!body.first || !body.last) {
     // Sends a HTTP bad request error code
-    return res.status(400).json({ data: 'First or last name not found' })
+    return res.status(200).json({ data: 'First or last name not found' })
   }
 
   // Found the name.
