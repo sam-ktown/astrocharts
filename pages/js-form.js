aad5de834270e241d31ae3b785ceecabeafdb985
sam-ktown <116414057+sam-ktown@users.noreply.github.com>
2022-10-22 00:52:09 -0700

    Update js-form.js

--- pages/js-form.js
+++ pages/js-form.js
@@ -36,7 +36,7 @@ export default function PageWithJSbasedForm() {
     // Get the response data from server as JSON.
     // If server returns the name submitted, that means the form works.
     //const result = await response.json()
-    alert(`Is this your full name: ${response.message}`)
+    //alert(`Is this your full name: ${response.message}`)
   }
   return (
     // We pass the event to the handleSubmit() function on submit.
