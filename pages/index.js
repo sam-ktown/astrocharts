34b7f145edf8ad8d216ab4aaebbc4952818b439d
sam-ktown <116414057+sam-ktown@users.noreply.github.com>
2022-10-21 22:18:34 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
+import DateInputForm from "@components/DateInputForm";
 
 export default function Home() {
   return (
@@ -18,7 +19,7 @@ export default function Home() {
         <p className="description">
           Birth Data Entry:
         </p>
-        <FeedbackForm />
+        <DateInputForm />
       </main>
       <Footer />
     </div>
